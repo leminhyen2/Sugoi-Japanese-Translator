2c26c708d210fae106af21c08c08ec79dfe50f5e
Blu3train <frtempra@gmail.com>
2021-06-18 10:38:41 +0200

    Added parameterized navigation timeout

--- backendServer/DeepLtranslator.js
+++ backendServer/DeepLtranslator.js
@@ -26,6 +26,8 @@ class DeepL {
         this.page = await this.browser.newPage();
         
         await this.page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+        await this.page.setDefaultNavigationTimeout(userSettings.DefaultNavigationTimeout); 
+        console.log( "* Set default navigation timeout to " + userSettings.DefaultNavigationTimeout + " msec *" )
         await this.page.goto(`https://www.deepl.com/en/translator#${this.originalLanguage}/${this.translationLanguage}/`);
         await this.page.waitForSelector(this.inputTextboxID);
         await this.page.waitForSelector(this.resultTextboxID)
